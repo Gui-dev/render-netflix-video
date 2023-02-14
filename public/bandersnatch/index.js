5613aeb3deecda126678e4bc3157b804e1f07e5f
Gui-dev <adrianon10@gmail.com>
2023-02-14 03:53:47 -0300

    feat: add factory, to start js files

--- public/bandersnatch/index.js
+++ public/bandersnatch/index.js
@@ -2,7 +2,7 @@ const MANIFEST_URL = 'manifest.json'
 const localHost = ['127.0.0.1', 'localhost']
 
 async function main() {
-    const isLocal = !!~localHost.indexOf(window.location.hostname)  
+    const isLocal = !!~localHost.indexOf(window.location.hostname)
     console.log('isLocal?', isLocal)
     const manifestJSON = await (await fetch(MANIFEST_URL)).json()
     const host = isLocal ? manifestJSON.localHost : manifestJSON.productionHost
