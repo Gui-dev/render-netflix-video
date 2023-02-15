d46a68b41ec93f4d81734039d9bd15c85b81a7cf
Gui-dev <adrianon10@gmail.com>
2023-02-15 03:56:03 -0300

    feat: add helper method waitForQuestions and nextChunk

--- public/bandersnatch/src/video-media-player.js
+++ public/bandersnatch/src/video-media-player.js
@@ -1,9 +1,11 @@
 class VideoMediaPlayer {
-  constructor ({ manifestJSON, network }) {
+  constructor ({ manifestJSON, network, videoComponent }) {
     this.manifestJSON = manifestJSON
     this.network = network
+    this.videoComponent = videoComponent
     this.videoElement = null
     this.sourceBuffer = null
+    this.activeItem = {}
     this.selected = {}
     this.videoDuration = 0
   }
@@ -31,7 +33,28 @@ class VideoMediaPlayer {
       const selected = this.selected = this.manifestJSON.intro
       mediaSource.duration = this.videoDuration
       await this.fileDownload(selected.url)
+      setInterval(this.waitForQuestions.bind(this), 200)
     }
+  }
+
+  waitForQuestions () {
+    const currentTime = parseInt(this.videoElement.currentTime)
+    const option = this.selected.at === currentTime
+    if (!option) return
+    if (this.activeItem.url === this.selected.url) return // evita que a modal seja aberta 2x no mesmo segundo
+    this.videoComponent.configureModal(this.selected.options)
+    this.activeItem = this.selected
+  }
+
+  async nextChunk (data) {
+    const key = data.toLowerCase()
+    const selected = this.manifestJSON[key]
+    this.selected = {
+      ...selected,
+      at: parseInt(this.videoElement.currentTime + selected.at) // ajustar o tempo que a modal vai aparecer, baseado no tempo corrente
+    }
+    this.videoElement.play() //deixa o restante do video rodar enquanto o novo video baixa
+    await this.fileDownload(selected.url)
   }
 
   async fileDownload (url) {
@@ -50,7 +73,7 @@ class VideoMediaPlayer {
   setVideoPlayerDuration (finalURL) {
     const bars = finalURL.split('/')
     const [name, videoDuration] = bars[bars.length - 1].split('-')
-    this.videoDuration += videoDuration
+    this.videoDuration += parseFloat(videoDuration)
   }
 
   async processBufferSegments (allSegments) {
