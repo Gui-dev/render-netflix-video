e5b56d053f7713fb435b13667296b97985a5ec6d
Gui-dev <adrianon10@gmail.com>
2023-02-15 04:22:14 -0300

    feat: add helper method getProperResolution

--- public/bandersnatch/src/network.js
+++ public/bandersnatch/src/network.js
@@ -11,4 +11,25 @@ class Network {
     const response = await fetch(url)
     return response.arrayBuffer()
   }
+
+  async getProperResolution (url) {
+    const startMs = Date.now()
+    const response = await fetch(url)
+    await response.arrayBuffer()
+    const endMs = Date.now()
+    const durationInMs = (endMs - startMs)
+    const resolutions = [ // ao invés de calcular o throughPut, vamos calcular pelo tempo
+      { start: 3001, end: 20000, resolution: 144 }, // acima 3 segundo
+      { start: 901, end: 3000, resolution: 360 }, // até 3 segundo
+      { start: 0, end: 900, resolution: 720 }, // menos de 1 segundo
+    ]
+    const item = resolutions.find(item => {
+      return item.start <= durationInMs && item.end >= durationInMs
+    })
+    const LOWEST_RESOLUTION = 144
+    if (!item) {
+      return LOWEST_RESOLUTION
+    }
+    return item.resolution
+  }
 }
